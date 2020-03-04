eba5fd37c7debf2364a3e4e5a1c96a902ff64657
haomiao <635541412@qq.com>
2020-03-04 11:43:01 +0800

    add some basic files.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Button text={'CustomButton'}></Button>
+    </div>
+  );
+}
+
+export default App;
